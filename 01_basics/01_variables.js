4d69d478316b9204f32c5fed06f4e79427fd85b8
Akriti Sharma <akritisharma2344@gmail.com>
2026-07-15 10:22:31 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "Akriti@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 // not allowed
+accountEmail = "aaas@gmail.com"
+accountPassword = "12445455"
+accountCity = "HI"
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);
+
