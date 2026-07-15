4488d25b3af5b721669082877c65596508b49796
Akriti Sharma <akritisharma2344@gmail.com>
2026-07-15 10:37:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert(3 + 3) we are using node js, not browser
+console.log(3+3)
+
+console.log("Akriti")
+
+let name = "Akriti"
+let age = 24
+let loggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value(object)
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof "Akriti");
+console.log(typeof null);
+console.log(typeof undefined);
