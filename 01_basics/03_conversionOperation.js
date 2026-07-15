24792bc45a3b25236475bb03e751ab1560a80c5e
Akriti Sharma <akritisharma2344@gmail.com>
2026-07-15 12:04:20 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,19 +13,66 @@ console.log(valueInNumber);
 // null = 0;
 // undefine => NaN;
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "akriti" => true
 
 
-let somwNumber = 33
+// let somwNumber = 33
 
-let stringNumber = String(somwNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+// let stringNumber = String(somwNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+
+
+
+
+
+
+// ****************************** Operations **********************
+
+// let value = 3
+// let negValue = -value
+// console.log(value)
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "Hello"
+// let str2 = " Akriti"
+
+// let str3 = str1 + str2;
+// console.log(str3)
+
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
