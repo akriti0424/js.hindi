465b28805d426cea954f67f3c65c5e0f2a05ca07
Akriti Sharma <akritisharma2344@gmail.com>
2026-07-15 12:36:42 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+//primitive 
+
+// 7 types: string, numer, boolean, null, undefined, symbol
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 35877990009887766655n
+//referenced type also called non-primitive
+
+//Arrays, objects, functions
+
+const heroes = ["akriti", "pratik"]
+
+let myObj = {
+    name:"akriti",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
